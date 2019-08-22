607521591af721a42def089bc8534c913cb57f39
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2019-08-23 00:50:21 +0900

    Simple counter

--- event-crud-app/src/App.js
+++ event-crud-app/src/App.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, world</h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <h1>Hello, world</h1>
+
+//       <label htmlFor="bar">bar</label>
+//       <input
+//         type="text"
+//         onChange={() => {
+//           console.log("Clicked");
+//         }}
+//       />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
+  return <Counter />;
+};
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count:{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
