1b830d676911207c706a645830d9979e598b628b
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2019-08-23 01:08:38 +0900

    Implement reducer

---
+++ event-crud-app/src/reducers/index.js
@@ -0,0 +1,7 @@
+// 全てのReducerを統括するためのファイル
+// Reducer: State と Action から新しい State を作成する
+
+import { combineReducers } from "redux";
+import { count } from "./count";
+
+export default combineReducers({ count });
