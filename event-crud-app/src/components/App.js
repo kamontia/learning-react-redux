04185716f0d0cf8d627b77a68b1346eed03e098d
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2019-08-24 01:10:04 +0900

    Implement linkage of components with connect method

--- event-crud-app/src/components/App.js
+++ event-crud-app/src/components/App.js
@@ -1,48 +1,31 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <h1>Hello, world</h1>
+import { increment, decrement } from "../actions";
 
-//       <label htmlFor="bar">bar</label>
-//       <input
-//         type="text"
-//         onChange={() => {
-//           console.log("Clicked");
-//         }}
-//       />
-//     </React.Fragment>
-//   );
-// }
-
-const App = () => {
-  return <Counter />;
-};
-
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
-
-  handlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-
+class App extends Component {
   render() {
+    const props = this.props;
     return (
       <React.Fragment>
-        <div>count:{this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>count:{props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value });
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+});
+
+/* 以下のようにも記載可能だが、分かりづらい */
+// const mapDispatchToProps = { increment, decrement };
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
