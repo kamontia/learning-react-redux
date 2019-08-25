9a424e663e3231d86a4d0ce1d079e49331f05a81
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2019-08-25 17:30:39 +0900

    Implement new post

--- event-crud-app/src/components/events_index.js
+++ event-crud-app/src/components/events_index.js
@@ -21,7 +21,6 @@ class EventsIndex extends Component {
   }
 
   render() {
-    const props = this.props;
     return (
       <React.Fragment>
         <table>
--- event-crud-app/src/components/events_new.js
+++ event-crud-app/src/components/events_new.js
@@ -1,25 +1,84 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-// import { postEvent } from "../actions";
+import { Field, reduxForm } from "redux-form";
+
+import { postEvents } from "../actions";
 import { Link } from "react-router-dom";
 
 class EventsNew extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  renderField(field) {
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error }
+    } = field;
+
+    return (
+      <div>
+        {/* touched: 一度でもクリックするとTrueとなる */}
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    );
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvents(values);
+    this.props.history.push("/");
+  }
+
   render() {
-    // const props = this.props;
-    return <React.Fragment></React.Fragment>;
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div>
+          <Field
+            label="Title"
+            name="title"
+            type="text"
+            component={this.renderField}
+          />
+          <Field
+            label="Body"
+            name="body"
+            type="text"
+            component={this.renderField}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Submit" disabled={false}></input>
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
+    );
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  // postEvents: () => dispatch(postEvents())
+  postEvents: values => dispatch(postEvents(values))
 });
 
 /* 以下のようにも記載可能だが、分かりづらい */
-// const mapDispatchToProps = { increment, decrement };
+// const mapDispatchToProps = { postEvents };
+
+const validate = values => {
+  const errors = {};
+
+  if (!values.title) errors.title = "Enter a title";
+  if (!values.body) errors.body = "Enter a body";
+
+  return errors;
+};
 
 /* states と actions を connect を使って関連付ける */
 export default connect(
   null,
   mapDispatchToProps
-)(EventsNew);
+)(reduxForm({ validate, form: "eventNewForm" })(EventsNew));
